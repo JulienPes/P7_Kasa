11c435c345b3fb791d28c8976c2c92b95983faca
Julien <juju2nizza@gmail.com>
2022-11-02 20:55:45 +0100

    Mise en place React app

---
+++ Kasa/src/App.js
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Location from "./pages/Location";
+import Page404 from "./pages/Page404";
+
+function App() {
+  return (
+    <div className="App">
+    <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/location/:id" element={<Location />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
