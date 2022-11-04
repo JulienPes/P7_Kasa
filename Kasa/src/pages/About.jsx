b5f6b7c00accfa7b2f9f1d9bf0d90fa6eb939c61
Julien <juju2nizza@gmail.com>
2022-11-04 20:14:15 +0100

    Ajout questions.json + start about page + css

---
+++ Kasa/src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Banner from "../assets/Background-About.png";
+
+
+const About = () => {
+  return (
+      <div className="about-img">
+        <img src={Banner} alt="Montagne fond banner" className="about-image-banner"/>
+      </div>
+  )
+}
+
+export default About
