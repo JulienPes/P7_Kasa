2742e1fcbf90aa0c64d7ff2932cab040939b44f7
Julien <juju2nizza@gmail.com>
2022-11-06 22:41:42 +0100

    Ajout du link de retour acceuil

--- Kasa/src/pages/Page404.jsx
+++ Kasa/src/pages/Page404.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Page404 = () => {
   return (
     <section>
       <h1>404</h1>
       <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" >Retourner sur la page d'acceuil</Link>
     </section>
   )
 }
