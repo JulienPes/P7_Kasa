2448e9b17304d24a4495546ca07133c8e7b4904d
Julien <juju2nizza@gmail.com>
2022-11-06 22:29:26 +0100

    Création 404

---
+++ Kasa/src/pages/Page404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page404 = () => {
+  return (
+    <section>
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+    </section>
+  )
+}
+
+export default Page404
