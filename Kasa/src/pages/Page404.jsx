0672554c4b838f9a4dff189a0b24a63f85074039
Julien <juju2nizza@gmail.com>
2022-11-06 23:13:59 +0100

    Ajout css page 404 terminée

--- Kasa/src/pages/Page404.jsx
+++ Kasa/src/pages/Page404.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Page404 = () => {
   return (
-    <section>
-      <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <Link to="/" >Retourner sur la page d'acceuil</Link>
+    <section className="section-404">
+      <h1 className="title-404">404</h1>
+      <p className="p-404">Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/" className="a-404">Retourner sur la page d'acceuil</Link>
     </section>
   )
 }
