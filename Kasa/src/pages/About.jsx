ace7b0c7e0a8d1c9975f6585a7ce6d776a466978
Julien <juju2nizza@gmail.com>
2022-11-09 22:38:08 +0100

    Débug about page

--- Kasa/src/pages/About.jsx
+++ Kasa/src/pages/About.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import { AboutImage } from "../assets";
+import { AboutImage } from '../assets';
 import { Accordeon } from '../components';
 import questions from "../data/questions.json";
 
 const About = () => {
   return (
-    <div>
+    <div className="about-page">
       <div className="about-img">
         <img src={AboutImage} alt="Bg banner" className="about-image-banner"/>
       </div>
-      
       <section className="about-section">
-        {questions.map(question => {
-          return <Accordeon question={question} key={question.id} />
-        })}
+        <div className="about-container">
+          {questions.map(question => {
+            return <Accordeon content={question} key={question.id} />
+          })}
+        </div>
       </section>
     </div>
   )
