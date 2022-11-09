0c9f95749a8b65e55eb967eaf206f301d1c229df
Julien <juju2nizza@gmail.com>
2022-11-09 18:54:48 +0100

    Création carousel

--- Kasa/src/assets/index.js
+++ Kasa/src/assets/index.js
@@ -1,3 +1,5 @@
+export { default as ChevronLeft } from "./Chevron_Left.svg";
+export { default as ChevronRight } from "./Chevron_Right.svg";
 export { default as ChevronDown } from "./Chevron_Down.svg";
 export { default as ChevronUp } from "./Chevron_Up.svg";
 export { default as FooterLogo } from "./Footer_Logo.png";
--- Kasa/src/components/LocationPageComponents/Carousel.jsx
+++ Kasa/src/components/LocationPageComponents/Carousel.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { ChevronLeft, ChevronRight } from '../../assets'
+import PropTypes from "prop-types";
 
-const Carousel = () => {
+const Carousel = ({ pictures }) => {
+  const [index, setIndex] = useState(0);
+
+  const showPreviousPhoto = () => {
+    if(index === 0) setIndex(pictures.length-1)
+    else setIndex(index-1);
+  }
+
+  const showNextPhoto = () => {
+    if(index === pictures.length-1) setIndex(0)
+    else setIndex(index+1)
+  }
+
   return (
-    <div>Carousel</div>
+    <div className="carousel">
+      <button className="previous-btn carousel-btn" onClick={showPreviousPhoto}>
+        <img src={ChevronLeft} alt="Previous" />
+      </button>
+      <img src={pictures[index]} alt="Logement" className="carousel-img"/>
+      <button className="next-btn carousel-btn" onClick={showNextPhoto}>
+        <img src={ChevronRight} alt="Next" />
+      </button>
+    </div>
   )
 }
+
+Carousel.propTypes = {
+  pictures: PropTypes.array,
+}
+
 export default Carousel
